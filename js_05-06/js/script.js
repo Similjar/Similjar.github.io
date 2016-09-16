cd0474de51f9e2d647ca0b2cec7d677fbb28a2e6
Violeta <similjar@gmail.com>
2016-09-17 01:50:20 +0300

    ДЗ js 05-06

--- js_05-06/js/script.js
+++ js_05-06/js/script.js
@@ -1,93 +1,77 @@
 'use strict';
-//alert('Домашнее задание JavaScript №03');
+//  Домашнее задание JavaScript №05-06
 (function () {
     var timer, boardTimer, split;
     var countTimer;
     var idTimer = 0;
-    var but = [];
+    var elemButton = [];
     var time = [0, 0, 0, 0];// hours, minutes, seconds, milisec;
+    
+    var elemHtml = { 
+        //--------- [tag, class, id]
+        divWrapper: ['div', 'wrapper',''], 
+        divMain: ['div', 'main',''],
+        divCanvas: ['canvas', 'board', 'timer'],
+        divButtons: ['div', 'btnClass',''],
+        tagButton: ['button', 'btn btn-default','btn-'],
+        divFooter: ['footer', 'footer',''],
+        divSplit: ['div', 'split', 'split'],
         
-    var width = '400px';
-    var indent = (parseInt(width)-time.length*50)/2;
-    var height = '100px';
-       
-    drawHtml();
-    timer.setAttribute('width', width);
-    timer.setAttribute('height', height);
-    boardTimer = timer.getContext('2d');
-    
-    drowBoardTimer();
-    countTimer = 0;
-   
-    but[0].onclick = function() {
-        if (but[0].getAttribute('class') === 'btn btn-default start') {
-            clearTimeout(idTimer);
-            idTimer = setInterval(drowTimer, 10);
-            console.log (idTimer);
-            but[0].className = 'btn btn-default stop';
-            but[0].innerHTML = 'stop';
-        } else if (but[0].getAttribute('class') === 'btn btn-default stop') {
-            clearTimeout(idTimer);
-            console.log (idTimer);
-            but[0].className = 'btn btn-default start';
-            but[0].innerHTML = 'start';
-        }
-    }
-    
-    but[1].onclick = function() {
-        var splitP = genTag.call(split, 'p', 'p');
-        splitP.innerHTML += ('split: '+ time[0] + ':' + time[1] + ':' + time[2] + ':' + time[3]);
-    }
-    
-    but[2].onclick = function() {
-        clearTimeout(idTimer);
-        countTimer = 0;
-        time = [0, 0, 0, 0];
-        but[0].className = 'btn btn-default start';
-        but[0].innerHTML = 'start';
-        drowBoardTimer();
-    }
-    
+        butName: ['start', 'split', 'reset', 'stop'],
         
-    function genTag(tagName, tagClass, tagId) {
-        var tag = document.createElement(tagName);
-        tag.className = tagClass;
-        if (tagId > '') {
-            tag.id = tagId;
-        }
-        this.appendChild(tag);
-        return tag;
+        htmlMinWidth: '400px',
+        htmlWidth: '400px',
+        htmlHeight: '100px',
+        
+        genTag: function (tagAtribut) {
+            var tag = document.createElement(tagAtribut[0]);
+            tag.className = tagAtribut[1];
+            if (tagAtribut[2] > '') {
+                tag.id = tagAtribut[2];
+            }
+            this.appendChild(tag);
+            return tag;
+        },
     }
     
     function drawHtml() { //tag, class, id
-        var wrapper = genTag.call(document.body, 'div', 'wrapper');
-        var main = genTag.call(wrapper, 'div', 'main');
-        timer = genTag.call(main, 'canvas', 'board', 'timer');
-        var battons = genTag.call(main, 'div', 'btnClass');
-        var footer = genTag.call(wrapper, 'footer', 'footer');
-        split = genTag.call(footer, 'div', 'split', 'split');
+        var wrapper = elemHtml.genTag.call(document.body, elemHtml.divWrapper);
+        var main = elemHtml.genTag.call(wrapper, elemHtml.divMain);
+        timer = elemHtml.genTag.call(main, elemHtml.divCanvas);
+        var buttons = elemHtml.genTag.call(main, elemHtml.divButtons);
+        var footer = elemHtml.genTag.call(wrapper, elemHtml.divFooter);
+        split = elemHtml.genTag.call(footer, elemHtml.divSplit);
         
-        var butName = ['start', 'split', 'reset'];
-        var butCount = butName.length;
-        for (var i = 0; i < butCount; i++){
-            but[i] = genTag.call(battons, 'button', 'btn btn-default ' + butName[i], 'but'+butName[i]); 
-            but[i].innerHTML = butName[i];
+        var butCount = elemHtml.butName.length;
+        var butAtr = [];
+        for (var i = 0; i < butCount-1; i++){
+            butAtr = ['button', 'btn btn-default','btn-'];
+            butAtr[1] = butAtr[1] +' ' + elemHtml.butName[i];
+            butAtr[2] = butAtr[2] + elemHtml.butName[i];
+            
+            elemButton[i] = elemHtml.genTag.call(buttons, butAtr);
+            elemButton[i].innerHTML = elemHtml.butName[i];
+            
         }
-        main.setAttribute('min-width', '400px');
-        main.setAttribute('width', '400px');
-   }
-    
-    function clear() { 
-        boardTimer.clearRect(0, 0, timer.width, timer.height);
+        main.setAttribute('min-width', elemHtml.htmlMinWidth);
+        main.setAttribute('width', elemHtml.htmlWidth);
     }
     
-    function drowBoardTimer(){
-        clear();
+    function clear() { 
+       boardTimer.clearRect(0, 0, timer.width, timer.height);
+    }
+    
+    function atribBoardTimer() {
         boardTimer.save();
         boardTimer.font = '36px Arial';
         boardTimer.fillStyle = '#000';
         boardTimer.textAlign = 'center';
         boardTimer.textBaseline = 'middle';
+    }
+    
+    function drawBoardTimer() {
+        clear();
+        atribBoardTimer();
         
         for (var i = 0; i < 4; i++){
             boardTimer.fillText('00', (i * 50) + indent + 25, 50);
@@ -98,13 +82,9 @@
     }
     
  // Основная функция
-    function drowTimer() { 
+    function drawTimer() { 
         clear();
-        boardTimer.save();
-        boardTimer.font = '36px Arial';
-        boardTimer.fillStyle = '#000';
-        boardTimer.textAlign = 'center';
-        boardTimer.textBaseline = 'middle';
+        atribBoardTimer();
 
         time[0] = Math.floor(countTimer / 3600000) % 24;
         var hourRest = countTimer % 3600000;
@@ -126,5 +106,46 @@
         countTimer += 10;
     }
     
-
+    var indent = (parseInt(elemHtml.htmlWidth)-time.length*50)/2;
+           
+    drawHtml();
+    
+    timer.setAttribute('width', elemHtml.htmlWidth);
+    timer.setAttribute('height', elemHtml.htmlHeight);
+    boardTimer = timer.getContext('2d');
+    drawBoardTimer();
+    
+    countTimer = 0;
+    
+    //start - stop
+    elemButton[0].onclick = function() {
+        if (elemButton[0].getAttribute('class') === (elemHtml.tagButton[1] + ' ' + elemHtml.butName[0])) {
+            clearTimeout(idTimer);
+            idTimer = setInterval(drawTimer, 10);
+            elemButton[0].className = elemHtml.tagButton[1] + ' ' + elemHtml.butName[3];
+            elemButton[0].innerHTML = elemHtml.butName[3];
+        } else if (elemButton[0].getAttribute('class') === (elemHtml.tagButton[1]  + ' ' + elemHtml.butName[3])) {
+            clearTimeout(idTimer);
+            elemButton[0].className = (elemHtml.tagButton[1] + ' ' + elemHtml.butName[0]);
+            elemButton[0].innerHTML = elemHtml.butName[0];
+            
+        }
+    }
+    
+    //split
+    elemButton[1].onclick = function() {
+        var splitP = elemHtml.genTag.call(split, 'p', 'p');
+        splitP.innerHTML += ('split: '+ time[0] + ':' + time[1] + ':' + time[2] + ':' + time[3]);
+    }
+    
+    //reset
+    elemButton[2].onclick = function() {
+        clearTimeout(idTimer);
+        countTimer = 0;
+        time = [0, 0, 0, 0];
+        elemButton[0].className = (elemHtml.tagButton[1] + ' ' + elemHtml.butName[0]);
+        elemButton[0].innerHTML = elemHtml.butName[0];
+        drawBoardTimer();
+    }
+        
 })();
