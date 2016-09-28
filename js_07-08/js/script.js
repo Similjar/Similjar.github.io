12c6940e5983d57f922db8482ca0477721db66d8
Violeta <similjar@gmail.com>
2016-09-28 20:50:34 +0300

    ДЗ js 07-08

---
+++ js_07-08/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+//  Домашнее задание JavaScript №07-08
+(function($) {
+$(function() {
+
+  $('.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    var indexTabContent = $(this).index();
+    var $tabsContent = $(this).closest('div.tabs').find('.tabs__content');
+    $tabsContent.removeClass('active');
+    $tabsContent.eq(indexTabContent).addClass('active');     
+  });
+    
+    $('.item__text').on("mouseenter mouseleave", function(){
+      $(this).next().toggleClass("hidden");
+    });   
+    
+    $('#butHelp').on('click', function() {
+      $(this).closest('ul.item__container').find('.form__item').find('.item__help').toggleClass("hidden");    
+        
+    });
+
+});
+})(jQuery);
+
